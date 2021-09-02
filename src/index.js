872b8a57c15859bbe22823c755d7237c3c0a49d5
Jay Clark <jayelizabethclark@gmail.com>
2021-09-02 16:36:46 -0400

    Form improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
-
 function App() {
+
     const [todos, setTodos] = React.useState([
         {
             text: 'Learn react',
@@ -20,14 +20,23 @@ function App() {
             checkbox: String.fromCharCode(9744),
             class: "todo",
         }
-    ]);
+        ]);
 
+    const [editFormData, setEditFormData] = React.useState(
+        {
+            value: '',
+            index: '',
+        }
+    )
 
     const addTodo = val => {
-        const newTodos = [...todos, {text: val, isCompleted: false, checkbox: String.fromCharCode(9744), class: "todo",}];
-        console.log(newTodos);
+        const newTodos = [{text: val, isCompleted: false, checkbox: String.fromCharCode(9744), class: "todo",},...todos];
         setTodos(newTodos);
     }
+
+    const [formDisplay, setFormDisplay] = React.useState('collapsed');
+
+    const [editFormDisplay, setEditFormDisplay] = React.useState('collapsed');
 
     const removeTodo = index => {
         let temp = [...todos];
@@ -48,11 +57,63 @@ function App() {
         setTodos(temp);
     }
 
+    const saveTodo = (value,index) => {
+        let temp = [...todos];
+        temp[index].text = value;
+        setTodos(temp);
+    }
+
+    const toggleForm = () => {
+
+        if (formDisplay == 'expanded') {
+            setFormDisplay('collapsed')
+        } else {
+            setFormDisplay('expanded'); 
+        }
+    }
+
+    const toggleEditForm = () => {
+        if (editFormDisplay == 'expanded') {
+            setEditFormDisplay('collapsed')
+        } else {
+            setEditFormDisplay('expanded');
+        }
+    }
+
+    const todoProps = {
+        complete: completeTodo,
+        remove: removeTodo,
+        save: saveTodo,
+        toggleEditForm: toggleEditForm,
+        setEditFormData: setEditFormData,
+    }
+
     return (
-        <div className="todo-list">
-            <div className="column-title-container"><div className="counter"><strong>{todos.length}</strong></div><div className="column-title"><strong>To do</strong></div></div>
-            {todos.map((todo,i) => <Todo key={i} todo={todo} index={i} complete={completeTodo} remove={removeTodo}/>)}
-            <TodoForm addTodo={addTodo}/>
+        <div>
+            <div className={editFormDisplay} style={{display:'flex',flexDirection:'row'}}>
+                <div className="edit-form">  
+                    <TodoFormEdit 
+                        save={saveTodo} 
+                        editFormDisplay={editFormDisplay} 
+                        setEditFormDisplay={setEditFormDisplay} 
+                        editFormData={editFormData}
+                        setEditFormData={setEditFormData}
+                        toggleEditForm={toggleEditForm} 
+                    />
+                </div>
+            </div>
+            <div className="todo-list">
+                <div className="column-title-container">
+                    <div className="counter"><strong>{todos.length}</strong></div>
+                    <div className="column-title"><strong>To do</strong></div>
+                    <div className="column-icon" onClick={toggleForm}><PlusIcon/></div>
+                    <div className="column-icon"><MoreIcon/></div>
+                </div>
+                <div className={formDisplay}>
+                    <TodoForm addTodo={addTodo} toggleForm={toggleForm} formDisplay={formDisplay} type='add'/>
+                </div>
+                {todos.map((todo,i) => <Todo props={todoProps} todo={todo} key={i} index={i} />)}
+            </div>
         </div>
     );
 }
--- src/todo.js
+++ src/todo.js
@@ -1,20 +1,136 @@
-function Todo({todo, index, remove, complete}){
+function Todo({todo, index, props}){
 
-    function handleRemove(){
-        remove(index);
-    }
+    const {complete, remove, save, toggleEditForm, setEditFormData} = props;
 
     function handleComplete(){
         complete(index);
     }
-    
+
     const checkID = "check-" + index;
+
+
+    const [formDisplay, setFormDisplay] = React.useState('collapsed');
+
+    const toggleForm = () => {
+
+        if (formDisplay == 'expanded') {
+            setFormDisplay('collapsed')
+        } else {
+            setFormDisplay('expanded'); 
+        }
+    }
+
+    const [menuDisplay, setMenuDisplay] = React.useState('collapsed');
+
+    const toggleMoreMenu = () => {
+        if (menuDisplay == 'expanded') {
+            setMenuDisplay('collapsed') 
+        }
+        else {
+            setMenuDisplay('expanded')
+        }
+    }
 
     return (
         <div className={todo.class} key={index} id={index}>
             <div id={checkID} onClick={handleComplete} className="card-icon"><CardIcon /></div>
-            <div className="todo-title">{todo.text}</div>
-            <div className="remove" onClick={handleRemove}><MoreIcon /></div>
+            <div style={{flexGrow:1}}>
+                <div style={{display:'flex',position:'relative',flexWrap:'nowrap',width:'100%'}} className={formDisplay == 'collapsed' ? 'expanded' : 'collapsed'}>
+                    <div className="todo-title">{todo.text}</div>
+                    <div className="remove" onClick={toggleMoreMenu}><MoreIcon /></div>
+                    <div className={menuDisplay} style={{zIndex:1000,position:'absolute',top:'20px',right:'-16px'}}>
+                        <MoreMenu remove={remove} toggleEditForm={toggleEditForm} index={index} setMenuDisplay={setMenuDisplay} value={todo.text} setEditFormData={setEditFormData}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
+}
+
+function MoreMenu({remove, toggleEditForm, index, value, setMenuDisplay, setEditFormData}) {
+
+    const handleEdit = () => {
+        setEditFormData({value,index});
+        toggleEditForm();
+        setMenuDisplay('collapsed');
+    }
+
+    const handleDelete = () => {
+        remove(index);
+        setMenuDisplay('collapsed');
+    }
+
+    return (
+            <div className="menu"><div className="menu-item" onClick={handleEdit}>Edit note</div>
+            <div className="menu-item" onClick={handleDelete}>Delete note</div></div>
+    )
+}
+
+function TodoFormEdit({save, editFormDisplay, setEditFormDisplay, editFormData, setEditFormData, toggleEditForm}) {
+   
+    const value = editFormData.value;
+
+    const [disabled, setDisabled] = React.useState(true);
+
+    const inputFocus = React.useRef(null);
+
+    const handleChange = e => {
+        let btnDisabled = true;
+        if (e.target.value !== '') {btnDisabled = false;}
+        setEditFormData({value: e.target.value, index: editFormData.index});
+        setDisabled(btnDisabled);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        save(value,editFormData.index);
+        setEditFormData({value:'',index:''});
+        setDisabled(true);
+        e.target.reset();
+        setEditFormDisplay('collapsed');
+    }
+
+    React.useEffect(()=>{
+        if (editFormDisplay == 'expanded') {inputFocus.current.focus();}
+    },[editFormDisplay, disabled])
+
+    return (
+        <div style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
+            <div className="edit-todo">
+                <div style={{borderRadius: '10px 10px 0px 0px', backgroundColor:'#F6f8fa',borderBottom:'1px solid rgba(186,188,190,0.4)',margin:'-16px -16px 0px -16px',padding:'16px',fontSize:'14px',fontWeight:'bold',display:'flex',width:'100%'}}>
+                    <div style={{flexGrow:1}}>Edit note</div><div style={{flexGrow:0}} onClick={toggleEditForm}><XIcon /></div>
+                </div>
+                <div style={{padding:'16px 0px 8px 0px',fontSize:'14px'}}><strong>Note</strong></div>
+                <form onSubmit={handleSubmit} style={{flexGrow:1}}>
+                <div style={{width:'100%'}}>
+                <textarea 
+                    type="text" 
+                    id="test-input"
+                    ref={inputFocus}
+                    autoComplete="off"
+                    className="input" 
+                    style={{padding:'8px',fontFamily:'inherit',fontSize:'14px',minWidth:'300px',maxWidth:'100%'}}
+                    value={editFormData.value} 
+                    onChange={handleChange}
+                    name="todo" />
+                </div>
+                <div className="button-wrapper">
+                    <div style={{flexGrow:1, width:'50%', margin:'0px 5px 0px 0px'}}>
+                        <div style={{position:'relative'}}>
+                            <div>
+                                <button id="submitBtn" style={{width: '100%',position:'absolute'}} className="btn btn-primary btn-grow" type="submit" disabled={disabled}>Save note</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div style={{position:'relative', flexGrow:1, width:'50%',margin:'0px 0px 0px 5px'}}>
+                    <button id="cancelBtn" className="btn btn-default btn-grow"  style={{width: '100%'}} type="button" onClick={toggleEditForm}>Cancel</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    );
+
 }
